acaa087a9d8f9ff75d4657bde362f183e8fca168
Rob-raspberry <r.ibarrabernal@outlook.com>
2022-04-16 10:47:35 -0700

    leds working

--- connection-2-server.py
+++ connection-2-server.py
@@ -1,5 +1,15 @@
 import socketio
-sio = socketio.Client(logger=True, engineio_logger=True)
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 21
+
+GPIO.setup(led,GPIO.OUT)
+
+sio = socketio.Client() #logger=True, engineio_logger=True
 
 @sio.event
 def connect():
@@ -11,10 +21,13 @@ def disconnect():
 
 @sio.event
 def order_taken(data):
+    GPIO.output(led,1)
+    time.sleep(0.5)
+    GPIO.output(led,0)
     print(data)
 
 def main():
-    sio.connect('http://localhost:3000')
+    sio.connect('http://airadrone.herokuapp.com')
     sio.emit("order",{"hola":"prueba"})
     sio.emit("order",{"hola":"prueba1"})
     sio.emit("order",{"hola":"prueba2"})
